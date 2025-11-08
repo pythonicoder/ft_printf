5e231fdb6574670dbd52f2bc735713d9f8d790b2
pythonicoder <159662601+pythonicoder@users.noreply.github.com>
2025-11-08 15:35:46 +0100

    Add files via upload

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skasikci <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/08 15:33:35 by skasikci          #+#    #+#             */
+/*   Updated: 2025/11/08 15:33:38 by skasikci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *format, ...);
+int	ft_formats(va_list args, char c);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putnbr(int n);
+int	ft_putunsigned(unsigned int n);
+int	ft_puthex(unsigned int n, char format);
+int	ft_putptr(unsigned long ptr);
+
+#endif
---
+++ ft_printf/ft_printf_utils_hex.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils_hex.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skasikci <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/08 15:31:21 by skasikci          #+#    #+#             */
+/*   Updated: 2025/11/08 15:33:03 by skasikci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	ft_putptr_rec(unsigned long ptr)
+{
+	int		len;
+	char	*base;
+
+	base = "0123456789abcdef";
+	len = 0;
+	if (ptr >= 16)
+		len += ft_putptr_rec(ptr / 16);
+	len += ft_putchar(base[ptr % 16]);
+	return (len);
+}
+
+int	ft_puthex(unsigned int n, char format)
+{
+	char	*base;
+	int		len;
+
+	if (format == 'x')
+		base = "0123456789abcdef";
+	else
+		base = "0123456789ABCDEF";
+	len = 0;
+	if (n >= 16)
+		len += ft_puthex(n / 16, format);
+	len += ft_putchar(base[n % 16]);
+	return (len);
+}
+
+int	ft_putptr(unsigned long ptr)
+{
+	int	len;
+
+	if (ptr == 0)
+		return (ft_putstr("(nil)"));
+	len = 0;
+	len += ft_putstr("0x");
+	len += ft_putptr_rec(ptr);
+	return (len);
+}
